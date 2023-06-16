c1b8892f7fd4c01cbb3c5f6e11d947ea24692b5a
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2023-06-16 15:20:27 +0200

    wip matrix rtation and distance

--- class/InstancedMeshClass.js
+++ class/InstancedMeshClass.js
@@ -1,62 +1,107 @@
 
 import * as THREE from 'three';
-
-export default class InstancedMeshClass{
-    constructor(parent, geometry, material, count, positions, rotations){
+import gsap from 'gsap'
+export default class InstancedMeshClass {
+    constructor(parent, geometry, material, count, from, to) {
         this.parent = parent;
         this.geometry = geometry;
         this.material = material;
         this.count = count;
-        this.positions = positions;
-        this.rotations = rotations;
+        // this.positions = positions;
+        // this.rotations = rotations;
         this.dummy = new THREE.Object3D();
-
+        this.from = from;
+        this.to = to;
         this.mesh = null;
 
         this.init();
     }
-    init(){
-        this.mesh = new THREE.InstancedMesh( this.geometry, this.material, this.count );
+    init() {
+        this.mesh = new THREE.InstancedMesh(this.geometry, this.material, this.count);
         this.setInstancedMeshPositions()
 
-        this.mesh.instanceMatrix.setUsage( THREE.DynamicDrawUsage ); //per animare
+        this.mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage); //per animare
+        if(this.parent.debug){
+            const axesHelper = new THREE.AxesHelper(5)
+            
+            this.mesh.add(axesHelper);
+        }
         this.parent.modelGroup.add(this.mesh);
 
-
+        // this.gsapAnimations()
         this.animate();
     }
-    setInstancedMeshPositions(){
-        for ( var i = 0; i < this.count; i ++ ) {
-            const rotationY = THREE.MathUtils.degToRad(360 / this.count * i);
+    setInstancedMeshPositions() {
+        const distnce = 3;
+        for (var i = 0; i < this.count; i++) {
+            const deg =360 / this.count * i
+            const radian = THREE.MathUtils.degToRad(deg)
+            const rotationY = radian;
+            console.log(deg)
             this.dummy.rotation.y = rotationY;
+            // this.dummy.translateX(Math.cos(radian) * distnce);
+            
+            // this.dummy.position.z = deg>= 10 ?  Math.cos(radian) * distnce :
+            // this.dummy.position.z = Math.sin(radian) * distnce
+            
             this.dummy.updateMatrix();
-            this.mesh.setMatrixAt( i, this.dummy.matrix );
+            this.mesh.setMatrixAt(i, this.dummy.matrix);
         }
         this.mesh.instanceMatrix.needsUpdate = true;
+
     }
 
-    randomizeMatrix(matrix) {
-        const position = new THREE.Vector3();
-        const rotation = new THREE.Euler();
-        const quaternion = new THREE.Quaternion();
-        const scale = new THREE.Vector3();
-
-        position.x = Math.random() * 40 - 20;
-        position.y = Math.random() * 40 - 20;
-        position.z = Math.random() * 40 - 20;
-
-        rotation.x = Math.random() * 2 * Math.PI;
-        rotation.y = Math.random() * 2 * Math.PI;
-        rotation.z = Math.random() * 2 * Math.PI;
-
-        quaternion.setFromEuler( rotation );
-        scale.x = scale.y = scale.z = Math.random() * 1;
-        matrix.compose( position, quaternion, scale );
+    gsapAnimations() {
+        const from = { posiiton: JSON.parse(JSON.stringify(this.dummy.position)) }
+        const tl = gsap.timeline();
+        const tempDummy = new THREE.Object3D()
+        tl.to(from.posiiton, {
+            duration: 5,
+            x: this.to.position.x,
+            onUpdate: () => {
+                console.log('position',from.posiiton.x)
+                for (var i = 0; i < this.count; i++) {
+                    tempDummy.position.set(from.posiiton)
+                    tempDummy.updateMatrix()
+                    this.mesh.setMatrixAt(i, tempDummy.matrix)
+                }
+                // this.mesh.instanceMatrix.needsUpdate = true;
+            }
+        }, 0)
+        
     }
 
-    animate(){
+    getSine(radius, deg){
 
+    }
 
-        requestAnimationFrame( () => this.animate() );
+    getCosine( radius, deg){
+
+    }
+    
+
+    // randomizeMatrix(matrix) {
+    //     const position = new THREE.Vector3();
+    //     const rotation = new THREE.Euler();
+    //     const quaternion = new THREE.Quaternion();
+    //     const scale = new THREE.Vector3();
+
+    //     position.x = Math.random() * 40 - 20;
+    //     position.y = Math.random() * 40 - 20;
+    //     position.z = Math.random() * 40 - 20;
+
+    //     rotation.x = Math.random() * 2 * Math.PI;
+    //     rotation.y = Math.random() * 2 * Math.PI;
+    //     rotation.z = Math.random() * 2 * Math.PI;
+
+    //     quaternion.setFromEuler( rotation );
+    //     scale.x = scale.y = scale.z = Math.random() * 1;
+    //     matrix.compose( position, quaternion, scale );
+    // }
+
+    animate() {
+
+        // console.log(this.dummy.position)
+        requestAnimationFrame(() => this.animate());
     }
 }
--- class/ThreeClass.js
+++ class/ThreeClass.js
@@ -4,8 +4,8 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'stats-js'
 import InstancedMeshClass from './InstancedMeshClass.js';
 
-export default class ThreeClass{
-    constructor(canvas){
+export default class ThreeClass {
+    constructor(canvas) {
         this.canvas = canvas;
         this.debug = true;
 
@@ -21,100 +21,103 @@ export default class ThreeClass{
         this.clock = null;
 
         this.modelGroup = new THREE.Group();
-        
+        this.layers = [
+            // { search: '01', quantity: 9, mesh : null},
+            { 
+                search: '02',
+                quantity: 1,
+                mesh: null,
+                from: {
+                    position: { 
+
+                    }
+                },
+                to: {
+                    position: { 
+                        x: 2, 
+                        y: 1,
+                        z: 1
+                    }, 
+                } 
+                },
+            // { search: '03', quantity: 9, mesh : null},
+            // { search: '04', quantity: 6, mesh : null},
+            // { search: '05', quantity: 6, mesh : null},
+            // { search: '06', quantity: 3, mesh : null},
+        ]
         this.init(canvas);
     }
-    async init(canvas){
+    async init(canvas) {
         this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color( 0xffffff );
-        this.camera = new THREE.PerspectiveCamera( 75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000 );
+        this.scene.background = new THREE.Color(0xffffff);
+        this.camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
         this.camera.position.z = 7;
-        
+
         this.initRenderer()
         this.clock = new THREE.Clock();
 
-        if(this.debug){
-            this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+        if (this.debug) {
+            this.controls = new OrbitControls(this.camera, this.renderer.domElement);
             this.stats = new Stats();
             const statsContainer = document.getElementById('stats');
-            if(statsContainer) statsContainer.appendChild(this.stats.dom)
+            if (statsContainer) statsContainer.appendChild(this.stats.dom)
         }
 
         this.initLights();
 
         this.gltf = await this.loadModel();
 
-        const piano_1 = this.gltf.scene.getObjectByName('foglia-agave-01'); 
-        const piano_2 = this.gltf.scene.getObjectByName('foglia-agave-02'); 
-        const piano_3 = this.gltf.scene.getObjectByName('foglia-agave-03'); 
-        const piano_4 = this.gltf.scene.getObjectByName('foglia-agave-04'); 
-        const piano_5 = this.gltf.scene.getObjectByName('foglia-agave-05'); 
-        const piano_6 = this.gltf.scene.getObjectByName('foglia-agave-06'); 
 
-        const piano_1_instancedMesh = new InstancedMeshClass(this, piano_1.geometry, piano_1.material, 9);
-        const piano_2_instancedMesh = new InstancedMeshClass(this, piano_2.geometry, piano_2.material, 9);
-        const piano_3_instancedMesh = new InstancedMeshClass(this, piano_3.geometry, piano_3.material, 9);
-        const piano_4_instancedMesh = new InstancedMeshClass(this, piano_4.geometry, piano_4.material, 6);
-        const piano_5_instancedMesh = new InstancedMeshClass(this, piano_5.geometry, piano_5.material, 6);
-        const piano_6_instancedMesh = new InstancedMeshClass(this, piano_6.geometry, piano_6.material, 3);
-        this.agave_cuore();
+        this.agave();
 
         this.scene.add(this.modelGroup)
         this.animate();
     }
 
-    agave_cuore(){
-        const agave_01 = this.gltf.scene.getObjectByName('agave-01001'); 
-        const agave_02 = this.gltf.scene.getObjectByName('agave-02001'); 
-        const agave_03 = this.gltf.scene.getObjectByName('agave-03001'); 
-        const agave_04 = this.gltf.scene.getObjectByName('agave-04001'); 
-        const agave_05 = this.gltf.scene.getObjectByName('agave-05001'); 
-        const agave_06 = this.gltf.scene.getObjectByName('agave-06001'); 
-
+    agave() {
         const agave_cuore = new THREE.Group();
-
-        agave_cuore.add(agave_01)
-        agave_cuore.add(agave_02)
-        agave_cuore.add(agave_03)
-        agave_cuore.add(agave_04)
-        agave_cuore.add(agave_05)
-        agave_cuore.add(agave_06);
+        this.layers.map((layer) => {
+            const piano = this.gltf.scene.getObjectByName(`foglia-agave-${layer.search}`);
+            layer.mesh = new InstancedMeshClass(this, piano.geometry, piano.material, layer.quantity, layer.from ,layer.to);
+            // const agave = this.gltf.scene.getObjectByName(`agave-${layer.search}001`); 
+            // agave_cuore.add(agave)
+        })
 
         this.modelGroup.add(agave_cuore);
     }
 
-    initRenderer(){
+    initRenderer() {
         const topPlane = new THREE.Plane(new THREE.Vector3(0, -1, 0), 4.3)
         const bottomPlane = new THREE.Plane(new THREE.Vector3(0, -1, 0), 0)
         bottomPlane.negate();
 
 
-        this.renderer = new THREE.WebGLRenderer({canvas:this.canvas});
-        this.renderer.setSize( this.canvas.clientWidth, this.canvas.clientHeight );
+        this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+        this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
         this.renderer.clippingPlanes = [topPlane, bottomPlane];
         this.renderer.localClippingEnabled = true;
 
     }
 
-    initLights(){
-        const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
-        this.scene.add( ambientLight );
+    initLights() {
+        const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+        this.scene.add(ambientLight);
 
-        const light_1 = new THREE.PointLight( 0xffffff, 1, 100 );
-        light_1.position.set( 0, 10, 0 );
-        if(this.debug){
-            const axesHelper = new THREE.AxesHelper( 5 );
-            light_1.add( axesHelper );
+        const light_1 = new THREE.PointLight(0xffffff, 1, 100);
+        light_1.position.set(0, 10, 0);
+        if (this.debug) {
+            const axesHelper = new THREE.AxesHelper(5);
+            light_1.add(axesHelper);
         }
 
-        this.modelGroup.add( light_1 );
+        this.modelGroup.add(light_1);
     }
 
-    async loadModel(){
-        return await new Promise((resolve,reject) => {
+    async loadModel() {
+        return await new Promise((resolve, reject) => {
             const loader = new GLTFLoader();
             return loader.load(
-                './models/agave-pianta.glb', 
+                './models/agave-pianta.glb',
                 (gltf) => {
                     return resolve(gltf);
                 },
@@ -122,33 +125,33 @@ export default class ThreeClass{
                     // console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
                 },
                 (error) => {
-                    console.log( 'An error happened' );
+                    console.log('An error happened');
                 }
             )
-            
+
         })
     }
 
-    handleAnimations(animations){
+    handleAnimations(animations) {
         animations.forEach((clip) => {
-            this.mixer.clipAction( clip ).play();
-        } );
+            this.mixer.clipAction(clip).play();
+        });
     }
 
-    animate(){
-        if(this.stats) this.stats.begin();
-        this.renderer.render( this.scene, this.camera );
+    animate() {
+        if (this.stats) this.stats.begin();
+        this.renderer.render(this.scene, this.camera);
 
         // this.mixer.update(this.clock.getDelta());
 
-        if(this.debug){
+        if (this.debug) {
             this.controls.update();
 
         }
 
-        if(this.stats) this.stats.end();
+        if (this.stats) this.stats.end();
 
-        requestAnimationFrame( () => this.animate() );
+        requestAnimationFrame(() => this.animate());
 
     }
 }
