b63c5fd291a33edc7308528f0144c24fce85ec05
Umberto Conti <umberto@gothamsiti.it>
2023-07-03 17:06:08 +0200

    silo

--- class/AnimationsClass.js
+++ class/AnimationsClass.js
@@ -12,7 +12,7 @@ export default class AnimationsClass {
             { name: 'agave', goAfter: null, timeFinished: 0, offsetStart: 0 },
             { name: 'oven', goAfter: 'agave', timeFinished: 0, offsetStart: -2 },
             { name: 'ovenBase', goAfter: 'agave', timeFinished: 0, offsetStart: -3 },
-            { name: 'silo', goAfter: 'ovenBase', timeFinished: 0, offsetStart: -3 }
+            { name: 'silo', goAfter: 'ovenBase', timeFinished: 0, offsetStart: -7 }
         ]
         this.parent.scene.traverse(child => {
             if (child.gsapAnimation) {
@@ -27,35 +27,28 @@ export default class AnimationsClass {
 
 
         this.animationTurns.map(item => item.timeFinished = this.calcTimeFinished(item.name))
-
         this.animationTurns.map((turn) => {
             for (let i in this.animations[turn.name]) {
-                this.masterTimeline.add(this.animations[turn.name][i], turn.goAfter ? this.animationTurns.find(item => item.name == turn.goAfter).timeFinished + turn.offsetStart : 0)
-                // console.log('animations', turn.name,this.animations[turn.name][0].labels  , turn.goAfter,turn.goAfter ?  this.animations[turn.goAfter][0].labels : 0)
+                var time = 0;
+                if(turn.goAfter){
+                    const prev = this.animationTurns.find(item => item.name == turn.goAfter);
+                    if(prev !== undefined) time = prev.timeFinished + turn.offsetStart;
+                }
+                this.masterTimeline.add(this.animations[turn.name][i], time)
             }
         })
-
-
-        // for(var i in this.animations){
-        //     for(var k in this.animations[i]){
-        //         if(i == 'agave'){  
-        //             console.log('agave', 'k',k)
-
-        //             this.masterTimeline.add(this.animations[i][k],0)
-        //         }
-        //         if(i == 'oven'){
-        //             console.log('oven', 'k',k)
-        //             this.masterTimeline.add(this.animations[i][k], 2.46 )
-        //         }
-        //     }
-        // }
     }
     calcTimeFinished(name, duration) {
         const turn = this.animationTurns.find((item) => item.name == name)
-        let turnDuration = turn.duration;
-        if (!turn.goAfter) {
-            return turnDuration
+        if(turn){
+            let turnDuration = turn.duration;
+
+            if (!turn.goAfter) {
+                return turnDuration
+            }
+            return turnDuration + this.calcTimeFinished(turn.goAfter, turn.duration)
         }
-        return turnDuration + this.calcTimeFinished(turn.goAfter, turn.duration)
+        return 0
+
     }
 }
