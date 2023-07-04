d6a692fe8553948388027b7124cff297616bcd52
Umberto Conti <umberto@gothamsiti.it>
2023-07-04 14:01:26 +0200

    new animationClass

--- class/AnimationsClass.js
+++ class/AnimationsClass.js
@@ -13,9 +13,9 @@ export default class AnimationsClass {
 
         this.regia = [
             {name: 'agave', offsetStart: 0, inited: false},
-            {name: 'ovenBase', offsetStart: -4, inited: false},
-            // {name: 'oven', offsetStart: -2, inited: false},
-            // {name: 'silo', offsetStart: -7, inited: false}
+            {name: 'ovenBase', offsetStart: -3, inited: false},
+            {name: 'oven', offsetStart: -11, inited: false},
+            {name: 'silo', offsetStart: -1, inited: false}
         ]
 
         this.init();
@@ -36,90 +36,33 @@ export default class AnimationsClass {
 
         this.masterTl = gsap.timeline({repeat: -1});
 
-        for(var c in this.coreografie){
-            this.addToTimeline(this.coreografie[c])
-        }
-    }
-    addToTimeline(coreografia){
-        for(var i in this.regia){
-            if(coreografia.context.inited === false){
-                setTimeout(() => {
-                    this.addToTimeline(coreografia)
-                }, 100);
-                return;
-            }
-
-            if(this.regia[i].name != coreografia.name) continue;
-            if(!this.regia[i].inited){
-                var position = null;
-                if(this.regia[i].offsetStart){
-                    position = this.regia[i].offsetStart > 0 ? '>+'+this.regia[i].offsetStart : '>'+this.regia[i].offsetStart
-                }
-
-                console.log(position)
-                this.masterTl.add(coreografia.tween(coreografia.context),position)
-                this.masterTl.addLabel(coreografia.name)
-                this.regia[i].inited = true;
-            }else{
-                const labelStart = this.masterTl.labels[coreografia.name];
-                if(labelStart){
-                    this.masterTl.add(coreografia.tween(coreografia.context),coreografia.name+'-='+labelStart)
-                }
+        for(var o in this.regia){
+            for(var c in this.coreografie){
+                if(this.regia[o].name != this.coreografie[c].name) continue;
+                this.addToTimeline(this.coreografie[c], this.regia[o], o)
             }
         }
     }
-
-
-
-    // constructor(parent) {
-    //     this.masterTimeline = gsap.timeline({ repeat: -1 })
-    //     this.parent = parent;
-    //     this.animations = {};
-    //     this.init();
-    //     this.animationTurns = []
-    // }
-    // init() {
-    //     this.animationTurns = [
-    //         { name: 'agave', goAfter: null, timeFinished: 0, offsetStart: 0 },
-    //         { name: 'oven', goAfter: 'agave', timeFinished: 0, offsetStart: -2 },
-    //         { name: 'ovenBase', goAfter: 'agave', timeFinished: 0, offsetStart: -3 },
-    //         { name: 'silo', goAfter: 'ovenBase', timeFinished: 0, offsetStart: -7 }
-    //     ]
-    //     this.parent.scene.traverse(child => {
-    //         if (child.gsapAnimation) {
-    //             if (!this.animations[child.gsapAnimation.name]) {
-    //                 this.animations[child.gsapAnimation.name] = []
-    //             }
-    //             this.animations[child.gsapAnimation.name].push(child.gsapAnimation)
-    //             const turn = this.animationTurns.find(item => item.name == child.gsapAnimation.name);
-    //             if(turn) turn.duration = child.gsapAnimation.labels[child.gsapAnimation.name]
-    //         }
-    //     })
-
-
-    //     this.animationTurns.map(item => item.timeFinished = this.calcTimeFinished(item.name))
-    //     this.animationTurns.map((turn) => {
-    //         for (let i in this.animations[turn.name]) {
-    //             var time = 0;
-    //             if(turn.goAfter){
-    //                 const prev = this.animationTurns.find(item => item.name == turn.goAfter);
-    //                 if(prev !== undefined) time = prev.timeFinished + turn.offsetStart;
-    //             }
-    //             this.masterTimeline.add(this.animations[turn.name][i], time)
-    //         }
-    //     })
-    // }
-    // calcTimeFinished(name, duration) {
-    //     const turn = this.animationTurns.find((item) => item.name == name)
-    //     if(turn){
-    //         let turnDuration = turn.duration;
-
-    //         if (!turn.goAfter) {
-    //             return turnDuration
-    //         }
-    //         return turnDuration + this.calcTimeFinished(turn.goAfter, turn.duration)
-    //     }
-    //     return 0
-
-    // }
+    addToTimeline(coreografia, turno, index){
+        if(coreografia.context.inited === false){
+            setTimeout(() => {
+                this.addToTimeline(coreografia, turno, index)
+            }, 100);
+            return;
+        }
+        if(!this.regia[index].inited){
+            var position = 0;
+            if(turno.offsetStart !== undefined){
+                position = turno.offsetStart >= 0 ? '>+'+turno.offsetStart : '>'+turno.offsetStart
+            }
+            this.masterTl.add(coreografia.tween(coreografia.context), position)
+            this.masterTl.addLabel(coreografia.name)
+            turno.inited = true;
+        }else{
+            const labelStart = this.masterTl.labels[coreografia.name];
+            if(labelStart){
+                this.masterTl.add(coreografia.tween(coreografia.context),coreografia.name+'-='+labelStart)
+            }
+        }
+    }
 }
