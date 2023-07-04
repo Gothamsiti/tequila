79ad065b021677816a61f3705d69fd27f0f957f6
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2023-07-04 14:51:21 +0200

    feat: forse ce la si fa

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-source "$HOME/.nvm/nvm.sh"
-nvm use 16.18.1
-node -v
-npm ci
-# Build the project
-npm run build
-
-# Serve the project using a process manager (e.g., PM2)
-pm2 start ecosystem.config.js --only tquilaDevelopment
