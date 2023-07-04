2b70b953d5810d06d7f3bf6d0ebb1f6a5b4a32e6
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2023-07-04 13:17:11 +0200

    test github actions

---
+++ pages/playGround.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+main#main
+    canvas(ref="mainThree")
+    #stats
+</template>
+<script setup>
+import ThreeClass from '../class/ThreeClass';
+const mainThree = ref();
+onMounted(() => {
+    const threeClass = new ThreeClass(false, mainThree.value);
+
+})
+
+</script>
+<style lang="scss">
+#main{
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+
+    canvas {
+        position: absolute;
+        top:0;
+        left:0;
+        width: 100%;
+        height: 100%;
+    }
+    > #stats{
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+}
+
+</style>
