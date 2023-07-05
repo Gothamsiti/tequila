f69a5235c62f6adcd1a6bb9e40122103ba4f4ef2
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2023-07-05 12:52:16 +0200

    feat: deleted component splash

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,15 +8,27 @@ export default defineNuxtConfig({
         }
     ]
   },
-  
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: '@import "@/assets/scss/vars.scss";\n',
-                },
-            },
-        },
+
+  buildModules: [
+    '@nuxtjs/pwa',
+  ],
+  pwa: {
+    icon: {
+      soure: '/images/icons/icons.png',
+      sizes: [ 64, 120, 144, 152, 192, 384, 512]
     },
+    meta: {
+      viewport:'width=device-width, initial-scale=1'
+    }
+  }, 
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  additionalData: '@import "@/assets/scss/vars.scss";\n',
+              },
+          },
+      },
+  },
   devtools: { enabled: false },
 })
