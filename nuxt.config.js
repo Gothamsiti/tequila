422f089cf505c1c7ac92761ad7a9c81946fae249
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2023-07-05 13:39:29 +0200

    feat: test vite

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,27 +18,70 @@ export default defineNuxtConfig({
     '@vite-pwa/nuxt',
   ],
   pwa: {
-    icon: {
-      soure: '/images/icons/icons.png',
-      sizes: [ 64, 120, 144, 152, 192, 384, 512]
-    },
-    meta: {
-      viewport:'width=device-width, initial-scale=1'
-    },
+    registerType: 'autoUpdate',
     manifest: {
-      "name": "Tequila",
-      useWebmanifestExtension: false
-
+      name: 'Nuxt Vite PWA',
+      short_name: 'NuxtVitePWA',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          "src": "images/icons/icon-72x72.png",
+          "sizes": "72x72",
+          "type": "image/png"
+        },
+        {
+          "src": "images/icons/icon-96x96.png",
+          "sizes": "96x96",
+          "type": "image/png"
+        },
+        {
+          "src": "images/icons/icon-128x128.png",
+          "sizes": "128x128",
+          "type": "image/png"
+        },
+        {
+          "src": "images/icons/icon-144x144.png",
+          "sizes": "144x144",
+          "type": "image/png"
+        },
+        {
+          "src": "images/icons/icon-152x152.png",
+          "sizes": "152x152",
+          "type": "image/png"
+        },
+        {
+          "src": "images/icons/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "images/icons/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "images/icons/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ],
     },
-  }, 
-  vite: {
-      css: {
-          preprocessorOptions: {
-              scss: {
-                  additionalData: '@import "@/assets/scss/vars.scss";\n',
-              },
-          },
-      },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
   },
   devtools: { enabled: false },
 })
