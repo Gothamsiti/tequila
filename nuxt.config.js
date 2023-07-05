80c080f0739679182b12bb558aedef58a7b1fa35
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2023-07-05 13:28:38 +0200

    feat: test vite pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,33 +9,28 @@ export default defineNuxtConfig({
     ]
   },
   head: {
-    links: [
-      {rel: "manifest", href: "/" }
+    link: [
+      {rel: "manifest", href: "/manifest.json" }
     ]
   },
 
-  buildModules: [
-    '@nuxtjs/pwa',
+  modules: [
+    '@vite-pwa/nuxt',
   ],
-  // pwa: {
-  //   icon: {
-  //     soure: '/images/icons/icons.png',
-  //     sizes: [ 64, 120, 144, 152, 192, 384, 512]
-  //   },
-  //   meta: {
-  //     viewport:'width=device-width, initial-scale=1'
-  //   },
-  //   manifest: {
-  //     "name": "Tequila",
-  //     "short_name": "Tequila",
-  //     "theme_color": "#2196f3",
-  //     "background_color": "#2196f3",
-  //     "display": "browser",
-  //     "orientation": "portrait",
-  //     "scope": "/",
-  //     "start_url": "/",
-  //   }
-  // }, 
+  pwa: {
+    icon: {
+      soure: '/images/icons/icons.png',
+      sizes: [ 64, 120, 144, 152, 192, 384, 512]
+    },
+    meta: {
+      viewport:'width=device-width, initial-scale=1'
+    },
+    manifest: {
+      "name": "Tequila",
+      useWebmanifestExtension: false
+
+    },
+  }, 
   vite: {
       css: {
           preprocessorOptions: {
