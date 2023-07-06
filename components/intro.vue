3c45d4bdf1fe3aefef2debc05845e655edf63c51
Maksim Leali <maksimleali@Maksims-Mac-mini.local>
2023-07-06 11:16:24 +0200

    fix: timeline stop

---
+++ components/intro.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+#intro(:class="[{vanish: vaish}]")
+    .text(v-html="'Intro di tequila'")
+    #startButton
+        span(v-html="'Inizia'")
+
+</template>
+
+<script>
+const vanish = ref(false)
+
+</script>
+<style lang="scss">
+#intro{
+    z-index: 998;
+    opacity: 1;
+    background-color: $white;
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: mw(2);
+    &.vanish {
+        animation: vanish 2s ease-in-out;
+        z-index: 0;
+        opacity: 0;
+    }
+}
+#startButton {
+    color: $white;
+    padding: 5px 20px;
+    background:$blueAgave;
+    border-radius: 50px;
+
+}
+
+</style>
